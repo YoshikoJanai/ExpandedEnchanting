199fdeb2caa6d074f300704e0267e5ccc774d9ae
YoshikoJanai <39793714+YoshikoJanai@users.noreply.github.com>
2020-09-15 01:46:15 -0400

    Update to 1.16.3. If I'm gonna continue this attempt, why not do it in the latest version

--- src/main/java/net/yoshikojanai/expandedenchanting/Enchantments/ExplosiveBowEnchant.java
+++ src/main/java/net/yoshikojanai/expandedenchanting/Enchantments/ExplosiveBowEnchant.java
@@ -2,9 +2,33 @@ package net.yoshikojanai.expandedenchanting.Enchantments;
 
 import net.minecraft.enchantment.EnchantmentTarget;
 import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
+
+import java.util.Map;
 
 public class ExplosiveBowEnchant extends ExpandedEnchantment {
     protected ExplosiveBowEnchant(Weight weight, EnchantmentTarget type, EquipmentSlot[] slotTypes) {
         super(weight, type, slotTypes);
+    }
+
+    @Override
+    public Tier getEnchantmentTier() {
+        return Tier.One;
+    }
+
+    @Override
+    public Map<EquipmentSlot, ItemStack> getEquipment(LivingEntity livingEntity) {
+        return super.getEquipment(livingEntity);
+    }
+
+    @Override
+    public int getMinimumLevel() {
+        return 1;
+    }
+
+    @Override
+    public int getMaximumLevel() {
+        return 3;
     }
 }
